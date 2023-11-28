ef50c5487809493da78c112e736ef459c8d37f8a
TacoTribe <152121498+TacoTribe@users.noreply.github.com>
2023-11-28 23:54:12 +0700

    Update nav-border-mobile.js

--- nav-border-mobile.js
+++ nav-border-mobile.js
@@ -1,5 +1,5 @@
-    // Mendapatkan elemen section
-    var section = document.getElementById("stickyNavMobile");
+    // Mendapatkan elemen section untuk mobile
+    var sectionMobile = document.getElementById("stickyNavMobile");
 
     // Menambahkan event listener untuk mendeteksi scroll
     window.addEventListener("scroll", function() {
@@ -9,12 +9,24 @@
       // Menentukan offset yang diinginkan (50px dalam hal ini)
       var offset = 50;
 
-      // Memeriksa apakah posisi scroll melebihi offset
+      // Memeriksa apakah posisi scroll melebihi offset untuk mobile
       if (scrollPosition > offset) {
-        // Jika iya, tambahkan class with-border pada elemen section
-        section.classList.add("with-border");
+        // Jika iya, tambahkan class with-border pada elemen section mobile
+        sectionMobile.classList.add("with-border");
       } else {
-        // Jika tidak, hilangkan class with-border pada elemen section
-        section.classList.remove("with-border");
+        // Jika tidak, hilangkan class with-border pada elemen section mobile
+        sectionMobile.classList.remove("with-border");
+      }
+
+      // Mendapatkan elemen section untuk desktop
+      var sectionDesktop = document.getElementById("stickyNav");
+
+      // Memeriksa apakah posisi scroll melebihi offset untuk desktop
+      if (scrollPosition > offset) {
+        // Jika iya, tambahkan class with-border pada elemen section desktop
+        sectionDesktop.classList.add("with-border");
+      } else {
+        // Jika tidak, hilangkan class with-border pada elemen section desktop
+        sectionDesktop.classList.remove("with-border");
       }
     });
