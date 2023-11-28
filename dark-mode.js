6922a2b6391663b8eaf6f40616bc0ee869003c0b
TacoTribe <152121498+TacoTribe@users.noreply.github.com>
2023-11-28 12:20:47 +0700

    Update dark-mode.js

--- dark-mode.js
+++ dark-mode.js
@@ -1,41 +1,30 @@
 function toggleMode(mode) {
-  const body = document.body;
-  const darkModeSwitch = document.getElementById('darkModeSwitch');
-  const whiteModeSwitch = document.getElementById('whiteModeSwitch');
+    const body = document.body;
+    const darkModeSwitch = document.getElementById('darkModeSwitch');
+    const whiteModeSwitch = document.getElementById('whiteModeSwitch');
 
-  if (mode === 'dark') {
-    body.classList.add('dark-mode');
-    darkModeSwitch.style.display = 'none';
-    whiteModeSwitch.style.display = 'inline-block';
-  } else if (mode === 'light') {
-    body.classList.remove('dark-mode');
-    darkModeSwitch.style.display = 'inline-block';
-    whiteModeSwitch.style.display = 'none';
-  } else if (mode === 'toggle') {
-    // Toggle between dark and light modes
-    if (body.classList.contains('dark-mode')) {
-      body.classList.remove('dark-mode');
-      darkModeSwitch.style.display = 'inline-block';
-      whiteModeSwitch.style.display = 'none';
-    } else {
+    if (mode === 'dark') {
       body.classList.add('dark-mode');
       darkModeSwitch.style.display = 'none';
       whiteModeSwitch.style.display = 'inline-block';
+    } else if (mode === 'light') {
+      body.classList.remove('dark-mode');
+      darkModeSwitch.style.display = 'inline-block';
+      whiteModeSwitch.style.display = 'none';
+    } else if (mode === 'toggle') {
+      // Toggle between dark and light modes
+      if (body.classList.contains('dark-mode')) {
+        body.classList.remove('dark-mode');
+        darkModeSwitch.style.display = 'inline-block';
+        whiteModeSwitch.style.display = 'none';
+      } else {
+        body.classList.add('dark-mode');
+        darkModeSwitch.style.display = 'none';
+        whiteModeSwitch.style.display = 'inline-block';
+      }
     }
   }
-}
 
-document.addEventListener('DOMContentLoaded', function () {
-  toggleMode('light');
-});
-
-// Tambahkan event listener untuk kedua span dengan fungsi yang sama
-document.getElementById('darkModeSwitch').addEventListener('click', function (event) {
-  toggleMode('toggle');
-  event.stopPropagation(); // Menghentikan propagasi agar klik tidak mencapai elemen a
-});
-
-document.getElementById('whiteModeSwitch').addEventListener('click', function (event) {
-  toggleMode('toggle');
-  event.stopPropagation(); // Menghentikan propagasi agar klik tidak mencapai elemen a
-});
+  document.addEventListener('DOMContentLoaded', function() {
+    toggleMode('light');
+  });
