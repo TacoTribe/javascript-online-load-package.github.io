44e2a98a1fecc011276b5da9f37c5c31f0f01c03
TacoTribe <152121498+TacoTribe@users.noreply.github.com>
2023-11-28 11:39:57 +0700

    Update dark-mode.js

--- dark-mode.js
+++ dark-mode.js
@@ -7,13 +7,24 @@
       body.classList.add('dark-mode');
       darkModeSwitch.style.display = 'none';
       whiteModeSwitch.style.display = 'inline-block';
-    } else {
+    } else if (mode === 'light') {
       body.classList.remove('dark-mode');
       darkModeSwitch.style.display = 'inline-block';
       whiteModeSwitch.style.display = 'none';
+    } else if (mode === 'toggle') {
+      // Toggle between dark and light modes
+      if (body.classList.contains('dark-mode')) {
+        body.classList.remove('dark-mode');
+        darkModeSwitch.style.display = 'inline-block';
+        whiteModeSwitch.style.display = 'none';
+      } else {
+        body.classList.add('dark-mode');
+        darkModeSwitch.style.display = 'none';
+        whiteModeSwitch.style.display = 'inline-block';
+      }
     }
   }
 
-    document.addEventListener('DOMContentLoaded', function() {
-    toggleMode('light'); 
-    });
+  document.addEventListener('DOMContentLoaded', function() {
+    toggleMode('light');
+  });
