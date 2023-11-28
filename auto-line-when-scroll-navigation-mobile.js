afb64b94f4502e55565fdd5df73187968dd0cb8f
TacoTribe <152121498+TacoTribe@users.noreply.github.com>
2023-11-28 22:26:37 +0700

    Update auto-line-when-scroll-navigation-mobile.js

--- auto-line-when-scroll-navigation-mobile.js
+++ auto-line-when-scroll-navigation-mobile.js
@@ -2,26 +2,27 @@ document.addEventListener("DOMContentLoaded", function () {
   var navigation = document.getElementById("stickyNavMobile");
   var content = document.querySelector(".content");
   var navOffset = navigation.offsetTop;
-  var isUpdating = false;
 
   function updateStickyNavigation() {
-    isUpdating = true;
-
     var isSticky = window.pageYOffset > navOffset;
 
     navigation.classList.toggle("sticky", isSticky);
     content.style.marginTop = isSticky ? navigation.offsetHeight + "px" : 0;
+  }
 
-    isUpdating = false;
+  // Menggunakan fungsi debounce untuk mengurangi frekuensi pemanggilan handleScroll
+  var debounceTimer;
+  function debounce(func, wait) {
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(func, wait);
   }
 
   function handleScroll() {
-    if (!isUpdating) {
-      requestAnimationFrame(updateStickyNavigation);
-    }
+    debounce(updateStickyNavigation, 10);
   }
 
-  window.addEventListener("scroll", handleScroll);
+  // Menggunakan passive:true untuk meningkatkan performa scroll
+  window.addEventListener("scroll", handleScroll, { passive: true });
 
-  updateStickyNavigation();
+  updateStickyNavigation(); // Pemanggilan awal agar kondisi awal di-handle
 });
