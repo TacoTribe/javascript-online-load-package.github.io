14f1add50f562cfcb3209d113767d2fd5918aff2
TacoTribe <152121498+TacoTribe@users.noreply.github.com>
2023-11-28 22:33:31 +0700

    Update auto-line-when-scroll-navigation-mobile.js

--- auto-line-when-scroll-navigation-mobile.js
+++ auto-line-when-scroll-navigation-mobile.js
@@ -1,33 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var navigation = document.getElementById("stickyNavMobile");
-  var content = document.querySelector(".content");
-  var navOffset = navigation.offsetTop;
+  const navigation = document.getElementById("stickyNavMobile");
+  const content = document.querySelector(".content");
+  const navOffset = navigation.offsetTop;
 
   function updateStickyNavigation() {
-    var isSticky = window.pageYOffset > navOffset;
+    const isSticky = window.scrollY > navOffset;
 
     navigation.classList.toggle("sticky", isSticky);
-    content.style.marginTop = isSticky ? navigation.offsetHeight + "px" : 0;
-  }
-
-  var debounceTimer;
-
-  function debounce(func, wait) {
-    clearTimeout(debounceTimer);
-    debounceTimer = setTimeout(func, wait);
+    content.style.marginTop = isSticky ? `${navigation.offsetHeight}px` : 0;
   }
 
   function handleScroll() {
-    debounce(updateStickyNavigation, 10);
-  }
-
-  function handleResize() {
-    navOffset = navigation.offsetTop; // Perbarui offset setelah perubahan ukuran jendela
     updateStickyNavigation();
   }
 
-  window.addEventListener("scroll", handleScroll, { passive: true });
-  window.addEventListener("resize", handleResize);
+  window.addEventListener("scroll", handleScroll);
 
   updateStickyNavigation();
 });
