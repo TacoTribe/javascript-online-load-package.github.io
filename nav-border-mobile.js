df5e1b07e58214edc47798b6abbfe090b9859fdc
TacoTribe <152121498+TacoTribe@users.noreply.github.com>
2023-11-28 23:07:33 +0700

    Create nav-border-mobile.js

---
+++ nav-border-mobile.js
@@ -0,0 +1,20 @@
+    // Mendapatkan elemen section
+    var section = document.getElementById("stickyNavMobile");
+
+    // Menambahkan event listener untuk mendeteksi scroll
+    window.addEventListener("scroll", function() {
+      // Mendapatkan posisi scroll
+      var scrollPosition = window.scrollY;
+
+      // Menentukan offset yang diinginkan (50px dalam hal ini)
+      var offset = 50;
+
+      // Memeriksa apakah posisi scroll melebihi offset
+      if (scrollPosition > offset) {
+        // Jika iya, tambahkan class with-border pada elemen section
+        section.classList.add("with-border");
+      } else {
+        // Jika tidak, hilangkan class with-border pada elemen section
+        section.classList.remove("with-border");
+      }
+    });
