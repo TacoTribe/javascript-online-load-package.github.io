2598e22ea24f14176e90974a61f7981af73d3702
TacoTribe <152121498+TacoTribe@users.noreply.github.com>
2023-11-28 22:45:36 +0700

    Update auto-line-when-scroll-navigation-mobile.js

--- auto-line-when-scroll-navigation-mobile.js
+++ auto-line-when-scroll-navigation-mobile.js
@@ -1,20 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var navigation = document.getElementById("stickyNavMobile");
-  var content = document.querySelector(".content");
-  var navOffset = navigation.offsetTop;
+const stickyNav = document.getElementById('stickyNavMobile');
+const content = document.querySelector('.content');
+const navOffset = stickyNav.offsetTop;
 
-  function updateStickyNavigation() {
-    var isSticky = window.pageYOffset > navOffset;
+const updateStickyNavigation = () => {
+  const isSticky = window.scrollY > navOffset;
 
-    navigation.classList.toggle("sticky", isSticky);
-    content.style.marginTop = isSticky ? navigation.offsetHeight + "px" : 0;
-  }
+  stickyNav.classList.toggle('sticky', isSticky);
+  content.style.marginTop = isSticky ? stickyNav.offsetHeight + 'px' : 0;
+};
 
-  function handleScroll() {
-    updateStickyNavigation();
-  }
+const handleScroll = () => updateStickyNavigation();
 
-  window.addEventListener("scroll", handleScroll);
+window.addEventListener('scroll', handleScroll);
 
-  updateStickyNavigation();
-});
+updateStickyNavigation();
