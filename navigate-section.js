fa634a130e6d83cdd2d9db83dba3b7805648f319
TacoTribe <152121498+TacoTribe@users.noreply.github.com>
2023-11-29 22:29:25 +0700

    Update navigate-section.js

--- navigate-section.js
+++ navigate-section.js
@@ -3,10 +3,12 @@ let currentSection = 1;
 function navigate(direction) {
   document.getElementById(`section${currentSection}`).classList.remove('activeSection');
 
+  const totalSections = document.querySelectorAll('.section').length;
+
   if (direction === 'back') {
-    currentSection = (currentSection - 1) < 1 ? document.querySelectorAll('.section').length : currentSection - 1;
+    currentSection = (currentSection - 1 + totalSections) % totalSections || totalSections;
   } else if (direction === 'next') {
-    currentSection = (currentSection + 1) > document.querySelectorAll('.section').length ? 1 : currentSection + 1;
+    currentSection = (currentSection % totalSections) + 1;
   }
 
   document.getElementById(`section${currentSection}`).classList.add('activeSection');
