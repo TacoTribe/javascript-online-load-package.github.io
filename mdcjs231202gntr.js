ac477e7e76c3c3d68074ef271967e280bad3a8f8
TacoTribe <152121498+TacoTribe@users.noreply.github.com>
2023-12-02 21:19:13 +0700

    Create mdcjs231202gntr.js

---
+++ mdcjs231202gntr.js
@@ -0,0 +1,132 @@
+ const imageCache = {};
+
+  async function downloadImage(url) {
+      if (imageCache[url]) {
+          return imageCache[url];
+      }
+
+      const response = await fetch(url);
+      const blob = await response.blob();
+      imageCache[url] = blob;
+      return blob;
+  }
+
+  async function loadImage(url) {
+      return new Promise((resolve, reject) => {
+          const img = new Image();
+          img.onload = () => resolve(img);
+          img.onerror = reject;
+          img.src = url;
+      });
+  }
+
+  function previewGambarFromURL(targetId) {
+
+  }
+
+  async function loadImageFromURL(url) {
+
+  }
+
+  function pilihLayer(event) {
+      const semuaOpsi = document.querySelectorAll('.layersGeneratorOption');
+      semuaOpsi.forEach((opsi) => {
+          opsi.classList.remove('active');
+      });
+      event.target.classList.add('active');
+
+      const urlLayer = event.target.src;
+      const layer = document.getElementById('layer');
+      layer.src = urlLayer;
+  }
+
+  const layerOptions = document.querySelectorAll('.layersGeneratorOption');
+  layerOptions.forEach((option) => {
+      option.addEventListener('click', pilihLayer);
+  });
+
+ function gambarUtamaURL() {
+    const inputGambarUtamaURL = document.getElementById('gambarUtamaURL');
+    const loading = document.getElementById('loading');
+    let namaFile = inputGambarUtamaURL.value.trim();
+
+    if (!namaFile) {
+      alert('You need to input ID');
+      return;
+    }
+
+    if (!namaFile.toLowerCase().endsWith('.png')) {
+      namaFile += '.png';
+    }
+
+    const baseURL = 'https://bafybeicngxlex3olbwwof2xtfsjareztc7gnkt6z56eqvdqujuuodp2kji.ipfs.nftstorage.link/';
+    const url = `${baseURL}${namaFile}`;
+
+    loading.style.display = 'block';
+
+    const img = new Image();
+    img.onload = function () {
+      const gambarUtama = document.getElementById('gambarUtama');
+      gambarUtama.src = url;
+      loading.style.display = 'none';
+    };
+    img.onerror = function () {
+      loading.style.display = 'none';
+      alert('Not found. Try another character.');
+    };
+    img.src = url;
+  }
+
+  async function gabungkanGambar() {
+      const gambarUtama = document.getElementById('gambarUtama');
+      const layer = document.getElementById('layer');
+
+      if (gambarUtama.src === '#' || layer.src === '#') {
+          alert('Please select the layers');
+          return;
+      }
+
+      const [blobGambarUtama, blobLayer] = await Promise.all([
+          downloadImage(gambarUtama.src),
+          downloadImage(layer.src),
+      ]);
+
+      const urlGambarUtama = URL.createObjectURL(blobGambarUtama);
+      const urlLayer = URL.createObjectURL(blobLayer);
+
+      const canvas = document.createElement('canvas');
+      const context = canvas.getContext('2d');
+
+      canvas.width = 1500;
+      canvas.height = 1500;
+
+      const imageGambarUtama = new Image();
+      imageGambarUtama.src = urlGambarUtama;
+      await new Promise((resolve) => {
+          imageGambarUtama.onload = resolve;
+      });
+      context.drawImage(imageGambarUtama, 0, 0, canvas.width, canvas.height);
+
+      const imageLayer = new Image();
+      imageLayer.src = urlLayer;
+      await new Promise((resolve) => {
+          imageLayer.onload = resolve;
+      });
+
+      context.drawImage(imageLayer, 0, 0, canvas.width, canvas.height);
+
+      const downloadLink = document.createElement('a');
+      canvas.toBlob(function (blob) {
+          const url = URL.createObjectURL(blob);
+          downloadLink.href = url;
+          downloadLink.download = 'sticker.png';
+          downloadLink.click();
+      });
+  }
+
+  window.addEventListener('load', function () {
+      var links = document.querySelectorAll('a.downloadSticker');
+      links.forEach(function (link) {
+          link.setAttribute('onclick', 'gabungkanGambar()');
+      });
+  });
