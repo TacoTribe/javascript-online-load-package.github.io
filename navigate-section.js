27a5b636668c59de1781f8ca900842441ad50924
TacoTribe <152121498+TacoTribe@users.noreply.github.com>
2023-12-04 22:35:02 +0700

    Update navigate-section.js

--- navigate-section.js
+++ navigate-section.js
@@ -1,43 +1,26 @@
+  let currentSection = 1;
+
+  function navigate(direction, targetSection) {
+    document.getElementById(`section${currentSection}`).classList.remove('activeSection');
+
+    if (direction === 'back') {
+      currentSection = Math.max(1, currentSection - 1);
+    } else if (direction === 'next') {
+      currentSection = Math.min(15, currentSection + 1);
+    } else {
+      currentSection = targetSection;
+    }
+
+    document.getElementById(`section${currentSection}`).classList.add('activeSection');
+  }
+
   document.addEventListener('DOMContentLoaded', function () {
-    let currentSection = 1;
-
-    function navigate(direction) {
-      document.getElementById(`section${currentSection}`).classList.remove('activeSection');
-
-      if (direction === 'back') {
-        currentSection = Math.max(1, currentSection - 1);
-      } else if (direction === 'next') {
-        currentSection = Math.min(15, currentSection + 1);
-      }
-
-      document.getElementById(`section${currentSection}`).classList.add('activeSection');
-    }
-
-    function getParameterByName(name, url) {
-      if (!url) url = window.location.href;
-      name = name.replace(/[\[\]]/g, '\\$&');
-      var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-      if (!results) return null;
-      if (!results[2]) return '';
-      return decodeURIComponent(results[2].replace(/\+/g, ' '));
-    }
-
-    const sectionParam = getParameterByName('on');
-    if (sectionParam) {
-      const sectionNumber = parseInt(sectionParam);
-      if (!isNaN(sectionNumber) && sectionNumber >= 1 && sectionNumber <= 15) {
-        currentSection = sectionNumber;
-      }
-    }
-
-    navigate(); // Set the initial state
-
-    // Optionally, you can call navigate() based on user actions, for example:
-    // document.getElementById('backButton').addEventListener('click', function () {
-    //   navigate('back');
-    // });
-    // document.getElementById('nextButton').addEventListener('click', function () {
-    //   navigate('next');
-    // });
+    // Add event listener to each navigation link
+    document.querySelectorAll('.subnavigasiLinks').forEach(function (link) {
+      link.addEventListener('click', function (event) {
+        event.preventDefault();
+        const targetSection = parseInt(this.getAttribute('href').replace('#section', ''));
+        navigate('goto', targetSection);
+      });
+    });
   });
