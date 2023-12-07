b90a076a7bfebc0f038e2d7ada059810793978f7
TacoTribe <152121498+TacoTribe@users.noreply.github.com>
2023-12-07 13:17:30 +0700

    Update dark-mode.js

--- dark-mode.js
+++ dark-mode.js
@@ -1,3 +1,13 @@
+// Fungsi untuk menyimpan mode ke local storage
+function saveModeToStorage(mode) {
+  localStorage.setItem('preferredMode', mode);
+}
+
+// Fungsi untuk memuat mode dari local storage
+function loadModeFromStorage() {
+  return localStorage.getItem('preferredMode');
+}
+
 function toggleMode(mode) {
   const body = document.body;
   const darkModeSwitch = document.getElementById('darkModeSwitch');
@@ -7,27 +17,32 @@ function toggleMode(mode) {
     body.classList.add('dark-mode');
     darkModeSwitch.style.display = 'none';
     whiteModeSwitch.style.display = 'inline-block';
+    saveModeToStorage('dark');
   } else if (mode === 'light') {
     body.classList.remove('dark-mode');
     darkModeSwitch.style.display = 'inline-block';
     whiteModeSwitch.style.display = 'none';
+    saveModeToStorage('light');
   } else if (mode === 'toggle') {
     // Toggle between dark and light modes
     if (body.classList.contains('dark-mode')) {
       body.classList.remove('dark-mode');
       darkModeSwitch.style.display = 'inline-block';
       whiteModeSwitch.style.display = 'none';
+      saveModeToStorage('light');
     } else {
       body.classList.add('dark-mode');
       darkModeSwitch.style.display = 'none';
       whiteModeSwitch.style.display = 'inline-block';
+      saveModeToStorage('dark');
     }
   }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  // Defaultnya menggunakan dark mode
-  toggleMode('dark');
+  // Memuat mode dari local storage atau menggunakan 'light' sebagai default
+  const preferredMode = loadModeFromStorage() || 'light';
+  toggleMode(preferredMode);
 });
 
 // Menambahkan event listener untuk masing-masing span
