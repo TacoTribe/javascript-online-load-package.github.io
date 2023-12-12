5752d5696ecd3eb49f03e5ecc216c4d36518d4e8
TacoTribe <152121498+TacoTribe@users.noreply.github.com>
2023-12-12 15:15:28 +0700

    Update dark-mode.js

--- dark-mode.js
+++ dark-mode.js
@@ -1,39 +1,40 @@
-// Fungsi untuk menyimpan mode ke local storage
 function saveModeToStorage(mode) {
   localStorage.setItem('preferredMode', mode);
 }
 
-// Fungsi untuk memuat mode dari local storage
 function loadModeFromStorage() {
   return localStorage.getItem('preferredMode');
 }
 
-// Fungsi untuk menangani perubahan mode
-function handleModeChange(mode) {
+function applyMode(mode) {
   const body = document.body;
   const darkModeSwitch = document.getElementById('darkModeSwitch');
   const whiteModeSwitch = document.getElementById('whiteModeSwitch');
 
-  body.classList.toggle('dark-mode', mode === 'dark');
-  darkModeSwitch.style.display = mode === 'dark' ? 'none' : 'inline-block';
-  whiteModeSwitch.style.display = mode === 'light' ? 'none' : 'inline-block';
+  if (mode === 'dark') {
+    body.classList.add('dark-mode');
+    darkModeSwitch.style.display = 'none';
+    whiteModeSwitch.style.display = 'inline-block';
+  } else {
+    body.classList.remove('dark-mode');
+    darkModeSwitch.style.display = 'inline-block';
+    whiteModeSwitch.style.display = 'none';
+  }
 
   saveModeToStorage(mode);
 }
 
-// Fungsi untuk menoggle antara dark dan light modes
 function toggleMode() {
-  const currentMode = loadModeFromStorage() || 'light';
-  const newMode = currentMode === 'light' ? 'dark' : 'light';
-  handleModeChange(newMode);
+  const body = document.body;
+  const currentMode = body.classList.contains('dark-mode') ? 'dark' : 'light';
+  const newMode = currentMode === 'dark' ? 'light' : 'dark';
+  applyMode(newMode);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  // Memuat mode dari local storage atau menggunakan 'light' sebagai default
-  const preferredMode = loadModeFromStorage() || 'light';
-  handleModeChange(preferredMode);
+  const preferredMode = loadModeFromStorage() || 'dark';
+  applyMode(preferredMode);
 });
 
-// Menambahkan event listener untuk masing-masing span
 document.getElementById('darkModeSwitch').addEventListener('click', toggleMode);
 document.getElementById('whiteModeSwitch').addEventListener('click', toggleMode);
