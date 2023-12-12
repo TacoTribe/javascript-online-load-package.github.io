77f233e1a5454f93a0304556c3c0dd92040a8c04
TacoTribe <152121498+TacoTribe@users.noreply.github.com>
2023-12-12 15:12:47 +0700

    Update dark-mode.js

--- dark-mode.js
+++ dark-mode.js
@@ -8,48 +8,32 @@ function loadModeFromStorage() {
   return localStorage.getItem('preferredMode');
 }
 
-function toggleMode(mode) {
+// Fungsi untuk menangani perubahan mode
+function handleModeChange(mode) {
   const body = document.body;
   const darkModeSwitch = document.getElementById('darkModeSwitch');
   const whiteModeSwitch = document.getElementById('whiteModeSwitch');
 
-  if (mode === 'dark') {
-    body.classList.add('dark-mode');
-    darkModeSwitch.style.display = 'none';
-    whiteModeSwitch.style.display = 'inline-block';
-    saveModeToStorage('dark');
-  } else if (mode === 'light') {
-    body.classList.remove('dark-mode');
-    darkModeSwitch.style.display = 'inline-block';
-    whiteModeSwitch.style.display = 'none';
-    saveModeToStorage('light');
-  } else if (mode === 'toggle') {
-    // Toggle between dark and light modes
-    if (body.classList.contains('dark-mode')) {
-      body.classList.remove('dark-mode');
-      darkModeSwitch.style.display = 'inline-block';
-      whiteModeSwitch.style.display = 'none';
-      saveModeToStorage('light');
-    } else {
-      body.classList.add('dark-mode');
-      darkModeSwitch.style.display = 'none';
-      whiteModeSwitch.style.display = 'inline-block';
-      saveModeToStorage('dark');
-    }
-  }
+  body.classList.toggle('dark-mode', mode === 'dark');
+  darkModeSwitch.style.display = mode === 'dark' ? 'none' : 'inline-block';
+  whiteModeSwitch.style.display = mode === 'light' ? 'none' : 'inline-block';
+
+  saveModeToStorage(mode);
+}
+
+// Fungsi untuk menoggle antara dark dan light modes
+function toggleMode() {
+  const currentMode = loadModeFromStorage() || 'light';
+  const newMode = currentMode === 'light' ? 'dark' : 'light';
+  handleModeChange(newMode);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   // Memuat mode dari local storage atau menggunakan 'light' sebagai default
-  const preferredMode = loadModeFromStorage() || 'dark';
-  toggleMode(preferredMode);
+  const preferredMode = loadModeFromStorage() || 'light';
+  handleModeChange(preferredMode);
 });
 
 // Menambahkan event listener untuk masing-masing span
-document.getElementById('darkModeSwitch').addEventListener('click', function() {
-  toggleMode('toggle');
-});
-
-document.getElementById('whiteModeSwitch').addEventListener('click', function() {
-  toggleMode('toggle');
-});
+document.getElementById('darkModeSwitch').addEventListener('click', toggleMode);
+document.getElementById('whiteModeSwitch').addEventListener('click', toggleMode);
