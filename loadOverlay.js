e2d8b22345015e724884c6074eb2591d1cccbe7f
TacoTribe <152121498+TacoTribe@users.noreply.github.com>
2023-12-12 12:31:20 +0700

    Update loadOverlay.js

--- loadOverlay.js
+++ loadOverlay.js
@@ -1,35 +1,43 @@
-  function createElementsFromJson(data) {
+async function createElementsFromJson(data) {
     const container = document.getElementById('layersGeneratorsContainer');
 
-    data.forEach(item => {
-      const optionMain = document.createElement('div');
-      optionMain.classList.add('OptionMain');
+    for (const item of data) {
+        const optionMain = document.createElement('div');
+        optionMain.classList.add('OptionMain');
 
-      const imgOption = document.createElement('div');
-      imgOption.classList.add('imgOption');
+        const imgOption = document.createElement('div');
+        imgOption.classList.add('imgOption');
 
-      const img = document.createElement('img');
-      img.classList.add('layersGeneratorOption');
-      img.src = item.imageUrl;
+        const img = document.createElement('img');
+        img.classList.add('layersGeneratorOption');
+        img.src = item.imageUrl;
 
-      imgOption.appendChild(img);
+        imgOption.appendChild(img);
 
-      const descriptionGenera = document.createElement('div');
-      descriptionGenera.classList.add('descriptionGenera');
+        const descriptionGenera = document.createElement('div');
+        descriptionGenera.classList.add('descriptionGenera');
 
-      const descriptionSpan = document.createElement('span');
-      descriptionSpan.textContent = item.description;
+        const descriptionSpan = document.createElement('span');
+        descriptionSpan.textContent = item.description;
 
-      descriptionGenera.appendChild(descriptionSpan);
+        descriptionGenera.appendChild(descriptionSpan);
 
-      optionMain.appendChild(imgOption);
-      optionMain.appendChild(descriptionGenera);
+        optionMain.appendChild(imgOption);
+        optionMain.appendChild(descriptionGenera);
 
-      container.appendChild(optionMain);
-    });
-  }
+        container.appendChild(optionMain);
+    }
+}
 
-  fetch('https://tacotribe.github.io/tacotribeoverlay.github.io/exportOverlay.json')
-    .then(response => response.json())
-    .then(jsonData => createElementsFromJson(jsonData))
-    .catch(error => console.error('Error fetching JSON:', error));
+async function fetchDataAndCreateElements() {
+    try {
+        const response = await fetch('https://tacotribe.github.io/tacotribeoverlay.github.io/exportOverlay.json');
+        const jsonData = await response.json();
+        await createElementsFromJson(jsonData);
+    } catch (error) {
+        console.error('Error fetching JSON:', error);
+    }
+}
+
+// Call the function to fetch data and create elements
+fetchDataAndCreateElements();
